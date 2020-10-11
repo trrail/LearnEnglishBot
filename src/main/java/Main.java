0e414495f58e668b67c5cb8c99ff46268b65fa24
Andrey0811 <0811andrey0811@mail.ru>
2020-10-12 01:19:53 +0500

    read token file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,21 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import transport.Reciever;
 import transport.Sender;
 
+import java.util.List;
+
 public class Main {
     private static final int prioritySender = 1;
     private static final int priorityReceiver = 3;
-    private static final String adminChatId = "407285071";
+    private static List<String> adminsChatId;
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
-        TelegramBot bot = new TelegramBot("indivisual_headman_bot",
-                "1345502933:AAHb4tkiiwgbvbHxPY_E8wLWMPgq82iITzU");
+
+        ReaderFile read = new ReaderFile(System.getProperty("user.dir") + "\\token.txt");
+        List<String> tokens = read.readLines();
+
+        TelegramBot bot = new TelegramBot(tokens.get(0), tokens.get(1));
+        setAdmin(tokens.subList(2, tokens.size()));
 
         Reciever reciever = new Reciever(bot);
         Sender messageSender = new Sender(bot);
@@ -34,9 +40,16 @@ public class Main {
     }
 
     private static void sendToAdmin(TelegramBot bot, String text) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(adminChatId);
-        sendMessage.setText(text);
-        bot.send.add(sendMessage);
+        for (String admin : adminsChatId) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(admin);
+            sendMessage.setText(text);
+            bot.send.add(sendMessage);
+        }
+
+    }
+
+    private static void setAdmin(List<String> ids){
+        adminsChatId = ids;
     }
 }
