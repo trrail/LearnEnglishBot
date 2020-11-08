bfdbdc85f0f8c0d6c378f30df141d92cd2e3b28e
trrail <trail.inproduct@yandex.ru>
2020-11-08 16:23:49 +0500

    ver 2

--- src/main/java/common/Word.java
+++ src/main/java/common/Word.java
@@ -1,15 +1,43 @@
 package common;
 
+import java.util.HashMap;
+
 public class Word {
     public String en;
     public String ru;
     public String example;
     public int frequency;
+    public HashMap<Long, Triple<Integer, Integer, Double>> dictionary;
 
     public Word(int freq, String en, String ru, String example){
         frequency = freq;
         this.en = en;
         this.ru = ru;
         this.example = example;
+        this.dictionary = new HashMap<>();
+    }
+
+    public Boolean checkFromRuToEn(Long userId, String query){
+        if (!this.dictionary.containsKey(userId))
+            this.dictionary.put(userId, new Triple<>(0, 0, 0.0));
+        if(query.equals(this.ru)){
+            calculateCoefficient(userId, true);
+            return true;
+        }
+        calculateCoefficient(userId,false);
+        return false;
+    }
+
+    private void calculateCoefficient(Long userId, Boolean bool){
+        int correct = this.dictionary.get(userId).getValue1();
+        int incorrect = this.dictionary.get(userId).getValue2();
+        if (bool) correct += 1;
+        else incorrect += 1;
+        this.dictionary.remove(userId);
+        this.dictionary.put(userId, new Triple<>(correct, incorrect, correct / incorrect * 100.0));
+    }
+
+    public double getCoefficient(Long userId){
+        return this.dictionary.get(userId).getValue3();
     }
 }
