41a9eda06549e2571fcdda46a620f14b481b9772
trrail <trail.inproduct@yandex.ru>
2020-11-15 22:47:53 +0500

    InlineKeyboard added + re-worked some methods in Selection

--- src/main/java/common/Selection.java
+++ src/main/java/common/Selection.java
@@ -16,41 +16,40 @@ public class Selection {
         // Если 0, то сортируется по убыванию значения incorrect в классе Word
     }
 
-    public Integer getWordStatistic(Long userId, String word){
+    public Integer getWordStatistic(User user, String word){
         // При создании incorrect в Word по умолчанию ставится 1
         for (Word lookingWord : this.words) {
             if (lookingWord.en.equals(word))
-                return lookingWord.getIncorrectAnswerStaticstic(userId);
+                return lookingWord.getIncorrectAnswerStaticstic(user.id);
         }
         return 0;
     }
 
-    public String getEnWord(Long userId){
+    public Word getEnWord(User user){
         Word returningWord = this.words.get(0);
-        int minimalIncStat = returningWord.getIncorrectAnswerStaticstic(userId);
+        int minimalIncStat = returningWord.getIncorrectAnswerStaticstic(user.id);
         for (Word lookingWord : this.words)
         {
-            if (lookingWord.getIncorrectAnswerStaticstic(userId) < minimalIncStat) {
-                minimalIncStat = lookingWord.getIncorrectAnswerStaticstic(userId);
+            if (lookingWord.getIncorrectAnswerStaticstic(user.id) < minimalIncStat) {
+                minimalIncStat = lookingWord.getIncorrectAnswerStaticstic(user.id);
                 returningWord = lookingWord;
             }
         }
-        return returningWord.en;
+        return returningWord;
     }
 
-    public Boolean checkTranslate(String enWord, String ruWord, Long userId){
-        assert getWordClass(enWord) != null;
-        boolean answer = getWordClass(enWord).checkFromRuToEn(userId, ruWord);
-        if (!this.usersStat.containsKey(userId))
-            this.usersStat.put(userId, new Tuple<>(0, 1));
-        Tuple<Integer, Integer> userStat = this.usersStat.get(userId);
+    public Boolean checkTranslate(String query, User user){
+        boolean answer = user.stateLearn.checkFromRuToEn(user.id, query);
+        if (!this.usersStat.containsKey(user.id))
+            this.usersStat.put(user.id, new Tuple<>(0, 1));
+        Tuple<Integer, Integer> userStat = this.usersStat.get(user.id);
         if (answer)
             userStat.setKey(userStat.getKey() + 1);
         else
             userStat.setValue(userStat.getValue() + 1);
         return answer;
     }
-
+    /*
     private Word getWordClass(String enWord){
         Word word = null;
         for (Word lookingWord : this.words)
@@ -60,9 +59,14 @@ public class Selection {
             }
         }
         return word;
+    }*/
+
+    public double getSelectionStatistic(User user){
+        return this.usersStat.get(user.id).getKey() * 100.0 / this.usersStat.get(user.id).getValue();
     }
 
-    public double getSelectionStatistic(Long userId){
-        return this.usersStat.get(userId).getKey() * 100.0 / this.usersStat.get(userId).getValue();
+    public void addWord(int freq, String en, String ru, String example){
+        Word word = new Word(freq, en, ru, example);
+        this.words.add(word);
     }
 }
